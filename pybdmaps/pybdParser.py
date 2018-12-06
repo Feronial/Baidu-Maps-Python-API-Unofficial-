94bfac190ba74a2c3e2003ae398b154a89007282
Feronial <mistiq.sonko@gmail.com>
2018-12-06 17:12:00 +0300

    Örnetklem koduna başlandı. (Input sample function)

--- pybdmaps/pybdParser.py
+++ pybdmaps/pybdParser.py
@@ -9,42 +9,79 @@ import re
 
 PARSER = re.compile(r'[,;| ]')
 
+def input_Sample(mode):
+
+	if mode == 'st' :
+		
+		print('Enter the input as format of Latitude, Longtitude . . .')
+		print('For string format : . . . . . . ')
+		print('"Latitude, Longtitude", \n  Example : "45.4545, 123.1233"')
+		print('For list format : . . . . . . ')
+		print(' [Latitude, Longtitude] \n Example : [45.4545, 123.1233]')
+		return 0
+	
+	
+	elif mode == 'brd':
+		
+		return 0
 
 #'brd' is border mode that means, user must give 4 points to determine rectanguler form
 # st is radiun mode that means, use must give 2 points tı determine circular form
 def location_Parser(location, mode = 'st'):
+
     
     if mode == 'st' : 
     
         type_Loc = type(location)
-    
+        
         if type_Loc == str:
         
-            if len(PARSER.split(location)) < 2:
+            if len(PARSER.split(location)) != 2:
             
                 raise Exception('Please Enter With [, ; | (space)] and There must be as least X, Y cordinates . . .')
                 return -1
         
-        
+            else:
+			
+                lat , lng = PARSER.split(location)
+                return lat, lng
         
         elif type_Loc == list:
         
-            if len(location) < 2:
+            if len(location) != 2:
             
                 raise Exception('Please Enter With [, ; | (space)] and There must be as least X, Y cordinates . . .')
                 return -1
         
             else:
             
-                lat, lng = location
-            
+                lat, lng = location            
                 return lat, lng
         
-    elif mode = 'brd':
+    elif mode == 'brd':
         
+        if type_Loc == str:
+            
+            if len(PARSER.split(location)) != 4:
+            
+                raise Exception('Please Enter With [, ; | (space)] and There must be as least X1, Y1, X2, Y2 cordinates . . .')
+                return -1
         
-    
-    
-    
-    
-    
+            else:
+			
+                lat , lng = PARSER.split(location)
+                return lat1, lng1, lat2, lng2
+        
+        elif type_Loc == list:
+        
+            if len(location) != 4:
+            
+                raise Exception('Please Enter With [, ; | (space)] and There must be as least X1, Y1, X2, Y2 cordinates . . .')
+                return -1
+        
+            else:
+            
+                lat1, lng1, lat2, lng2 = location            
+                return lat1, lng1, lat2, lng2
+
+	
